b8aaa41859631edd6c82d91930b3c197c87ad003
IllustraDesign <illustradesignstudio25@gmail.com>
2025-03-23 10:20:08 +0530

    project id is wrong

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { Client, Databases, Storage, ID } from "https://esm.sh/appwrite";
 
 const client = new Client();
-client.setEndpoint("https://cloud.appwrite.io/v1").setProject("67de7dd4000f29e394fb");
+client.setEndpoint("https://cloud.appwrite.io/v1").setProject("67def1fd000f20fb3cc7");
 
 const databases = new Databases(client);
 const storage = new Storage(client);
@@ -27,7 +27,7 @@ document.getElementById("productForm").addEventListener("submit", async (e) => {
         const imageUpload = await storage.createFile("67def576003bbab396a8", ID.unique(), imageFile);
 
         // Generate the correct image URL
-        const imageUrl = `https://cloud.appwrite.io/v1/storage/buckets/67def576003bbab396a8/files/${imageUpload.$id}/view?project=67de7dd4000f29e394fb`;
+        const imageUrl = `https://cloud.appwrite.io/v1/storage/buckets/67def576003bbab396a8/files/${imageUpload.$id}/view?project=67def1fd000f20fb3cc7`;
 
         // Store product in Appwrite Database (Ensure correct Database & Collection ID)
         console.log({
